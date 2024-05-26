d68afa491365abe64aa12855b3076a108b158543
ananth905 <85988246+ananth905@users.noreply.github.com>
2024-05-26 22:54:15 +0530

    wallpaper change

--- app/services/desktopservice.js
+++ app/services/desktopservice.js
@@ -3,5 +3,5 @@ import Service from '@ember/service';
 export default class DesktopserviceService extends Service {
   appName = '';
   wallpaper =
-    'https://149366088.v2.pressablecdn.com/wp-content/uploads/2024/03/ubuntu-24.04-wallpaper-840x473.jpg';
+    'https://getwallpapers.com/wallpaper/full/5/e/a/821188-korra-wallpapers-1920x1080-for-iphone-5s.jpg';
 }
