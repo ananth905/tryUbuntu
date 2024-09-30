76b7ba7ccd14a1d84026f95d9c3974b5fbf0b552
Ananth <anantharajmkn29@gmail.com>
2024-09-30 20:52:57 +0530

    Ephiny browser app created

---
+++ app/components/installed-apps/ephiny/web.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class Web extends Component {
+
+
+    constructor(){
+      super(...arguments);
+    }
+
+}
